aa43e4aab0595543d35f389a1b59c2bfacf0193a
Rudy Leiva <leivarudy93@gmail.com>
2025-06-08 21:04:09 -0700

    Add RequestMapping path for consistency

--- src/main/java/com/example/jsb/product/controllers/ProductController.java
+++ src/main/java/com/example/jsb/product/controllers/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("api/products")
 public class ProductController {
 
     private final CreateProductService createProductService;
@@ -44,39 +45,39 @@ public class ProductController {
     }
 
 
-    @PostMapping("/product")
+    @PostMapping
     public ResponseEntity<ProductDTO> createProduct(@RequestBody Product product){
         return createProductService.execute(product);
     }
 
-    @GetMapping(value = "/products")
+    @GetMapping
     public ResponseEntity<List<ProductDTO>> getProducts(){
         return getProductsService.execute(null);
     }
 
     // Mapping to get product by id
-    @GetMapping(value = "/product/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> getProductById(@PathVariable Integer id){
         return getProductService.execute(id);
     }
 
-    @GetMapping("/product/search")
+    @GetMapping("/search")
     public ResponseEntity<List<ProductDTO>> searchByProductName(@RequestParam String name) {
         return searchProductService.execute(name);
     }
 
-    @PutMapping("/product/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ProductDTO> updateProduct(@PathVariable Integer id, @RequestBody Product product){
         //pass in both id and product together
         return updateProductService.execute(new UpdateProductCommand(id, product));
     }
 
-    @PatchMapping("/product/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<ProductDTO> patchProduct(@PathVariable Integer id, @RequestBody JsonNode patchBody){
         return patchProductService.execute(new PatchProductCommand(id, patchBody));
     }
 
-    @DeleteMapping("/product/{id}") //id here must match id in line below
+    @DeleteMapping("/{id}") //id here must match id in line below
     public ResponseEntity<Void> deleteProduct(@PathVariable Integer id){
         return deleteProductService.execute(id);
     }
